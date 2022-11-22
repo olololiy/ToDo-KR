3625799651a4f0d82d3d05946fcf108026388a2d
artyom <artem.lysyansky@kirarock.team>
2022-11-22 11:41:17 +0300

    commit перед добавлением vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,31 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <main>
+    <TaskCard></TaskCard>
+  </main>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import TaskCard from "@/components/TaskCard";
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    TaskCard
   }
 }
 </script>
 
 <style>
-#app {
+* {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+main {
+  max-width: 450px;
+  margin: 0 auto;
 }
 </style>
---
+++ src/components/TaskCard.vue
@@ -0,0 +1,43 @@
+<template>
+
+  <v-card title="Card title" text="..." variant="outlined">
+    <v-card-actions>
+      <v-btn>Click me</v-btn>
+    </v-card-actions>
+  </v-card>
+
+  <div>
+    <div>
+      <h4>{{ model.title }}</h4>
+      <p>{{model.description}}</p>
+    </div>
+    <div>
+      <button v-if="!model.status">готово</button>
+      <button v-else>удалить</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    model: {
+      required: true,
+      default:{
+        id: 0,
+        title: 'defaultTitle',
+        description: 'defaultDesc',
+        status: false
+      },
+    }
+},
+setup()
+{
+
+}
+}
+</script>
+
+<style scoped>
+
+</style>
