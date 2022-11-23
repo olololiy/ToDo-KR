d7d5ef946883dba2c82792e2a53cbda86d97e8ec
artyom <artem.lysyansky@kirarock.team>
2022-11-23 15:36:31 +0300

    удаление работает

--- src/App.vue
+++ src/App.vue
@@ -4,28 +4,28 @@
       <v-text-field
           class="title"
           label="title"
-          v-model="title"
+          v-model="taskStore.title"
       >
       </v-text-field>
       <v-text-field
           class="description"
           label="description"
-          v-model="body"
+          v-model="taskStore.body"
       >
       </v-text-field>
       <v-btn
           class="add-btn"
-          @click="addTask"
+          @click="taskStore.addTask"
       >
         ADD
       </v-btn>
     </v-form>
-
-    <div class="task_list" v-for="task in tasks">
+<div>{{taskStore.tasks[1]}}</div>
+    <div class="task_list" v-for="task of taskStore.tasks">
       <v-card :title="task.title" :text="task.body" variant="outlined">
         <v-card-actions>
           <v-btn>Done</v-btn>
-          <v-btn @click="removeTask(task)">Delete</v-btn>
+          <v-btn @click="taskStore.removeTask(task)">Delete</v-btn>
         </v-card-actions>
       </v-card>
     </div>
@@ -33,75 +33,11 @@
   </main>
 </template>
 
-<script>
-import TaskCard from "@/components/TaskCard";
-import TaskInput from "@/components/TaskInput";
-import TaskList from "@/components/TaskList";
-import {ref, computed} from "vue";
+<script setup>
+import {useTaskStore} from "@/stores/StoreTask";
 
-export default {
-  name: 'App',
-  components: {
-    TaskInput,
-    TaskCard,
-    TaskList
-  },
-  setup(){
-    let tasks = ref([
-      {id: 1, title: "homework", body: 'do homework'},
-      {id: 2, title: "cleaning", body: 'do debug'},
-      {id: 3, title: "learning", body: 'do js'},
-    ]);
-    let title = ref('');
-    let body = ref('');
-    const addTask = () => {
-      let newTask = {
-        id: Date.now(),
-        title: title.value,
-        body: body.value
-      }
-      console.log(newTask)
-      tasks.value.push(newTask)
-      console.log("addTask")
-      title.value=''
-      body.value=''
-    };
-    const removeTask = task => {
-      console.log("removeTask")
-      console.log("до", tasks.value)
-      tasks.value = tasks.value.filter(item => item.id !== task.id)
-      console.log("после", tasks.value)
-    };
-    return{removeTask, addTask, tasks, title,body}
-  },
-  //
-  // data() {
-  //   return {
-  //     tasks: [
-  //       {id: 1, title: "homework", body: 'do homework'},
-  //       {id: 2, title: "cleaning", body: 'do debug'},
-  //       {id: 3, title: "learning", body: 'do js'},
-  //     ],
-  //     title: '',
-  //     body: ''
-  //   }
-  // },
-  // methods: {
-  //   addTask(){
-  //     let newTask = {
-  //       id: Date.now(),
-  //       title: this.title,
-  //       body: this.body
-  //     }
-  //     this.tasks.push(newTask)
-  //     this.title=''
-  //     this.body=''
-  //   },
-  //   removeTask(task){
-  //     this.tasks = this.tasks.filter(p => p.id !== task.id)
-  //   }
-  // },
-}
+const taskStore = useTaskStore()
+
 </script>
 
 <style>
--- src/stores/StoreTask.js
+++ src/stores/StoreTask.js
@@ -1,15 +1,14 @@
 import {defineStore} from "pinia";
-import {ref} from "vue";
+import {computed, ref} from "vue";
 
-export const useTaskStore = defineStore('taskStore',{})
-
-    let tasks = ref([
+export const useTaskStore = defineStore('taskStore',() => {
+    const tasks = ref([
         {id: 1, title: "homework", body: 'do homework'},
         {id: 2, title: "cleaning", body: 'do debug'},
         {id: 3, title: "learning", body: 'do js'},
     ]);
-    let title = ref('');
-    let body = ref('');
+    const title = ref('');
+    const body = ref('');
     const addTask = () => {
         let newTask = {
             id: Date.now(),
@@ -24,8 +23,34 @@ export const useTaskStore = defineStore('taskStore',{})
     };
     const removeTask = task => {
         console.log("removeTask")
-        console.log("до", tasks.value)
-        tasks.value = tasks.value.filter(item => item.id !== task.id)
+        let el = tasks.value.findIndex(item => item.id === task.id)
+        console.log("el:",el)
+        tasks.value.splice(el, 1)
+        console.log(task)
         console.log("после", tasks.value)
     };
     return{removeTask, addTask, tasks, title,body}
+    // const addTask = computed(()=>{
+    //     let newTask = {
+    //         id: Date.now(),
+    //         title: title.value,
+    //         body: body.value
+    //     }
+    //     console.log(newTask)
+    //     tasks.value.push(newTask)
+    //     console.log("addTask")
+    //     title.value=''
+    //     body.value=''
+    // })
+
+    // const removeTask = computed(()=>{
+    //     console.log("removeTask")
+    //     console.log("до", tasks.value)
+    //     tasks.value = tasks.value.filter(item => item.id !== task.id)
+    //     console.log("после", tasks.value)
+    // })
+
+
+})
+
+
